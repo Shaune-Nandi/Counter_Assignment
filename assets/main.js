db477e1191fd270b53a8eb3b9400ca1e85c5aad2
Shaune <shaunenandi27@gmail.com>
2022-06-23 10:06:18 +0300

    Added all 4 functionalities

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,30 @@
-const button5 = document.querySelector("#plusFive")
+const button5 = document.querySelector("#plusFive");
+const button10 = document.getElementById("plusTen");
+const button15 = document.getElementById("plusFifteen");
+const button20 = document.getElementById("plusTwenty");
+
 const indicator = document.querySelector("#number-indicator")
 const disp = document.querySelector("#display-heading")
 
+// Arrow fn
 button5.addEventListener('click', () => {
   disp.textContent = "added 5"
   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
+})
+
+button10.addEventListener('click', () => {
+  disp.textContent = "added 10"
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
+})
+
+
+// Normal fn
+button15.addEventListener('click', function add_15(){
+  disp.textContent = "added 15"
+  indicator.textContent = (parseInt(indicator.textContent) + 15).toString()
+})
+
+button20.addEventListener('click', function add_20(){
+  disp.textContent = "added 20"
+  indicator.textContent = (parseInt(indicator.textContent) + 20).toString()
 })
